3107d648264260022fbc49632071b9c84375b23e
Loan420512 <hongloancute1234@gmail.com>
2025-06-04 02:19:05 +0700

    Login

--- InfertilityTreatmentSystem/Controllers/HomeController.cs
+++ InfertilityTreatmentSystem/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
-using _1.Models;
+﻿using _1.Models;
+using InfertilityTreatmentSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace InfertilityTreatmentSystem.Controllers
 {
@@ -28,5 +29,30 @@ namespace InfertilityTreatmentSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            if (model.Username == "admin" && model.Password == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Tên đăng nhập hoặc mật khẩu không đúng.");
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
     }
 }
